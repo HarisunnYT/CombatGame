7435001ea5a432f887caa96e0efb499c1c171e86
Unknown <harisunnyt@gmail.com>
2020-07-29 15:36:24 +0930

    Projectile on client will ignore sender

--- Assets/_Game/Scripts/Combat/Projectile.cs
+++ Assets/_Game/Scripts/Combat/Projectile.cs
@@ -17,6 +17,8 @@ public class Projectile : NetworkBehaviour
 
     private float targetDestroyTime = -1;
 
+    private PlayerController sender;
+
     protected bool isHost { get { return (SteamLobbyManager.Instance && SteamLobbyManager.Instance.PublicHost) || (ServerManager.Instance && !ServerManager.Instance.IsOnlineMatch);  } }
 
     private void Awake()
@@ -28,6 +30,8 @@ public class Projectile : NetworkBehaviour
     private void OnEnable()
     {
         targetDestroyTime = ServerManager.Time + secondsTillDestroy;
+        //if (sender != null)
+        //    IgnoreSender(sender.PlayerID);
     }
 
     public virtual void AddForce(Vector3 spawnPosition, Vector3 direction, float force, ForceMode2D forceMode)
@@ -44,8 +48,20 @@ public class Projectile : NetworkBehaviour
 
     public void AddForce(PlayerController player, Vector3 direction, float force, ForceMode2D forceMode)
     {
-        Physics2D.IgnoreCollision(player.Collider, collider);
+        sender = player;
+        RpcIgnoreSender(player.PlayerID);
         AddForce(player.Collider.bounds.center, direction, force, forceMode);
+    }
+
+    [ClientRpc]
+    private void RpcIgnoreSender(int playerId)
+    {
+        IgnoreSender(playerId);
+    }
+
+    private void IgnoreSender(int playerId)
+    {
+        Physics2D.IgnoreCollision(ServerManager.Instance.GetPlayer(playerId).PlayerController.Collider, collider);
     }
 
     private void AddForce()
@@ -65,10 +81,7 @@ public class Projectile : NetworkBehaviour
 
     protected void Despawn()
     {
-        if (isHost)
-        {
-            gameObject.SetActive(false);
-            NetworkServer.UnSpawn(gameObject);
-        }
+        gameObject.SetActive(false);
+        sender = null;
     }
 }
